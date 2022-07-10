b2d3237c8f009a025c2f627ce8d9fc55bf2d1fd5
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-07-10 18:41:40 -0300

    fixed Interface

--- Interfaces/IApplication.cs
+++ Interfaces/IApplication.cs
@@ -2,10 +2,10 @@ namespace endpointManager
 {
     public interface IApplication
     {
-        void InsertEndpoint(){}
-        void EditEndpoint(){}
-        void DeleteEndpoint(){}
-        void ListEndpoints(){}
-        void SearchEndpoint(){}
+        void InsertEndpoint();
+        void EditEndpoint();
+        void DeleteEndpoint();
+        void ListEndpoints();
+        void SearchEndpoint();
     }
 }
